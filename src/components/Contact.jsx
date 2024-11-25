9ee8cde85c42a37c9c8fa4e2f135726078d45ee9
Yusri Nelson <yusrinelson17@gmail.com>
2024-11-25 09:51:57 +0200

    updated resume

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -53,7 +53,9 @@ const Contact = () => {
         </div>
       </div>
       <div className="w-full border-b-[1px] border-gray-500"/>
-      <div className="text-center py-2 text-[12px]"><p>Copyright &#169; 2024 Yusri Nelson, All Rights Reserved.</p></div>
+      <div className="text-center py-2 text-[12px]">
+        <p>Copyright &#169; 2024 Yusri Nelson, All Rights .Reserved</p>
+        </div>
     </div>
   );
 };
